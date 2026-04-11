59ed7689cbdb352e1c465324d1512e03fab6191e
gishee003 <kirtinigam003@gmail.com>
2026-04-11 12:01:14 +0530

    Added health endpoint in train.py and install curl command in its dockerfile

--- model_training/train.py
+++ model_training/train.py
@@ -75,5 +75,9 @@ def train_model():
         print("Model Training error:", e)
         return jsonify({"error": str(e)}), 500
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "ok"}), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
